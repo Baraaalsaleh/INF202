0c8dc09def1726420d31bd2c624df0818b3b8fe3
Baraaalsaleh <61140930+Baraaalsaleh@users.noreply.github.com>
2020-03-29 14:08:36 +0300

    Version 0

---
+++ src/berichtserstellungssystem/Manager.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package berichtserstellungssystem;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Baraa
+ */
+public class Manager extends Person{
+    ArrayList addedEmployees = new ArrayList<Employee>();
+    ArrayList addedTestUnits = new ArrayList<TestUnit>();
+    ArrayList addedCustomers = new ArrayList<Customer>();
+}
